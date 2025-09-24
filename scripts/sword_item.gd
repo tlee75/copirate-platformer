3d5a6a51dd61a5a559ab3a86e65fed4e6684e0c8
tlee75 <tlee75@asu.edu>
2025-09-23 18:04:42 -0700

    Fixed broken animation

--- scripts/sword_item.gd
+++ scripts/sword_item.gd
@@ -6,3 +6,12 @@ func action(user):
 	user.is_trigger_action = true
 	user.get_node("AnimatedSprite2D").play("attack")
 	
+	# Clean up any existing connections first
+	var anim_sprite = user.get_node("AnimatedSprite2D")
+	if anim_sprite.animation_finished.is_connected(user._on_attack_animation_finished):
+		anim_sprite.animation_finished.disconnect(user._on_attack_animation_finished)
+	if anim_sprite.animation_finished.is_connected(user._on_ground_animation_finished):
+		anim_sprite.animation_finished.disconnect(user._on_ground_animation_finished)
+
+	# Connect the attack finished signal
+	anim_sprite.animation_finished.connect(user._on_attack_animation_finished)
