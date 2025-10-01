03aa8af07b020e3b797fc8d9cf41b7ccfc4620dd
tlee75 <tlee75@asu.edu>
2025-10-01 00:08:46 -0700

    add assets

---
+++ scripts/berry_bush.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var full_texture: Texture2D
+@export var empty_texture: Texture2D
+var has_berries: bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if full_texture:
+		$Sprite2D.texture = full_texture
+
+
+func handle_interact_action(player):
+	if has_berries:
+		has_berries = false
+		
+
+func give_player_berries():
+	print("Player harvested berries")
