7f38b6380f71641ffb16a3cc8573d9c85ec1c1a3
tlee75 <tlee75@userse.noreply.github.com>
2026-03-28 18:56:50 -0700

    Add granite rock pile

---
+++ scripts/terrain/granite_rock_pile.gd
@@ -0,0 +1,24 @@
+extends GameObject
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Setup harvest
+	category = "terrain"
+	max_harvest = 3
+	harvest_remaining = max_harvest
+	regeneration_time = 5.0 
+	is_harvestable = true
+	is_destructible = false
+	harvest_loot = "simple_rock"
+	
+	target_actions = ["harvest"]
+	
+	# Start with normal idle animation
+	if animated_sprite:
+		animated_sprite.play("idle_full")
+
+	# Call parent _ready() which will setup hover detection
+	super._ready()
+
+func is_interactable() -> bool:
+	return is_harvestable == true
